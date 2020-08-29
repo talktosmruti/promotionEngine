5bb9f5b956637eec5a75f37a5df779c5420ab8f8
Smruti <talktosmruti@gmail.com>
2020-08-29 13:34:55 +0530

    msg1

--- src/main/java/com/promotion/engine/EngineApplication.java
+++ src/main/java/com/promotion/engine/EngineApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class EngineApplication {
 
 	public static void main(String[] args) {
+		System.out.println("hi");
 		SpringApplication.run(EngineApplication.class, args);
 	}
 
